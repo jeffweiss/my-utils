a1c56f342947db59a485b7e87e513d2eec607593
Michael Surma <michael.surma@hp.com>
2012-03-05 12:38:59 -0800

    updated version to push out automagically.
    Change-Id: I48daa9e73b4a66a36256dfbf92f02bd20277b78b

--- create_ssh_config_entries.pl
+++ create_ssh_config_entries.pl
@@ -1,19 +1,75 @@
 #!/usr/bin/perl
 
 use strict;
+use Getopt::Long;
+# We will be using expect to manage the login process for copying the SSH keys
+use Expect;
 
-while (<>) {
-   
-   chomp;
-   my $hostname = $_;
-   next if($hostname =~ /^\#/);
-   my ($host) = split(/\./, $hostname);
-   print "Host " . $host . "\n";
-   print "   User surma\n";
-   print "   HostName ", $hostname, "\n";
-   print "   Port 22\n";
-   print "   IdentityFile ~/.shh/id_rsa\n";
-   print "   ForwardAgent yes\n" if ($host =~ /redmars|plateng8/);
-   last if (eof());
-   print "\n";
+# Fancy password prompt
+use Term::ReadKey;
+
+
+$| = 0;
+print "Enter your Unix Username: ";
+my $user = ReadLine(0);
+
+chomp $user;
+
+print "Enter your password: ";
+ReadMode('noecho');
+my $password = ReadLine(0);
+
+chomp $password;
+ReadMode('normal');
+
+print "Enter the name of the server list text file: ";
+my $inputfile = ReadLine(0);
+chomp $inputfile;
+
+open('FH', "<$inputfile") or die "can't open $inputfile: $!";
+
+my $outputfile = "/home/$user/.ssh/testconfig";
+open OFH, ">>$outputfile" or die "can't open $outputfile: $!";
+
+# Loop through the file of hostnames one at a time
+while (defined (my $hostname = <FH>)) {
+  chomp $hostname;
+
+  # Check to see if key authentication is already working so we don't copy the key twice.
+  if(system("ssh -o BatchMode=yes -o ConnectTimeout=5 $hostname uptime 2>&1 | grep -q average") != "0")
+  {
+    # Set up the command to copy the ssh key
+    my $cmd = "ssh-copy-id -i $hostname";
+
+    # Print comfort text
+    print "Now copying key to $hostname";
+
+    # Set up expect and spawn a command
+    my $timeout = '10';
+    my $ex = Expect->spawn($cmd) or die "Cannot spawn $cmd\n";
+
+    # Look for the password prompt and send the password
+    $ex->expect($timeout, ["[pP]assword:"]);
+    $ex->send("$password\n");
+    $ex->soft_close();
+      printConfigData($hostname, $user);
+ } else { print "Key already deployed on $hostname\n" }
 }
+close('FH');
+close('OFH');
+
+sub printConfigData {
+
+      my $hostname = shift;
+      my $user = shift;
+
+      my ($host) = split(/\./, $hostname);
+      print OFH "Host " . $host . "\n";
+      print OFH "   User $user\n";
+      print OFH "   HostName ", $hostname, "\n";
+      print OFH "   Port 22\n";
+      print OFH "   IdentityFile ~/.shh/id_rsa\n"; 
+      print OFH "\n";
+      return;
+}
+
