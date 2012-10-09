b0071b7fde70ee7a09b705ab106dab889faacca9
Michael Surma <michael.surma@hp.com>
2012-10-09 11:47:58 -0700

    added hostgroups. cleaned up output file creation to support new functionality

--- nagios_hosts_cfg_gen.pl
+++ nagios_hosts_cfg_gen.pl
@@ -15,7 +15,8 @@ if (@ARGV == 1) {
 }
 
 my $nagioscfgpath = "/etc/nagios/configs";
-my $nagiosbasename = "-hosts.cfg";
+my $hostsbasename = "-hosts.cfg";
+my $hostgroupbasename = "-hostgroups.cfg";
 # end global vars section
 #
 # set STDOUT buffer to flush immediately
@@ -27,22 +28,25 @@ my $stack = ReadLine(0);
 chomp $stack;
 my $lstack = lc($stack);
 
-my $outputfile = $nagioscfgpath . "/" . $stack . "/" . $lstack . $nagiosbasename;
+my $hostsfile = $nagioscfgpath . "/" . $stack . "/" . $lstack . $hostsbasename;
+my $hostgroupfile = $nagioscfgpath . "/" . $stack . "/" . $lstack . $hostgroupbasename;
 if ($debug) {
-   $outputfile = "test.cfg";
+   $hostsfile = "hoststest.cfg";
+   $hostgroupfile = "hostgrouptest.cfg";
 }
 
+my @allhosts = ();
 
 # Connect to EC2 with my credentials and get me the stuff I need.
 #
 my $ec2 = Net::Amazon::EC2->new(
-	AWSAccessKeyId => 'YourAWSPublicKey',
-	SecretAccessKey => 'YourAWSPrivateKey',
+	AWSAccessKeyId => 'enter your pub key here',
+	SecretAccessKey => 'enter your private key here',
 );
 
 my $running_instances = $ec2->describe_instances;
 # Now that we have the EC2 object open the output file handle
-open OFH, ">>$outputfile" or die "Can't open $outputfile: $!";
+open OFH, ">>$hostsfile" or die "Can't open $hostsfile: $!";
 
 foreach my $reservation (@$running_instances) {
 	foreach my $instance ($reservation->instances_set) {
@@ -59,27 +63,44 @@ foreach my $reservation (@$running_instances) {
 			}
 		}
 	}
-  printConfigData($hostname, $ip_address, $tier, $stack);
+  printhostsConfigData($hostname, $ip_address, $tier, $stack);
+  push @allhosts, $hostname; 
 }
 close ('OFH');
 
-sub printConfigData {
+open HGOFH, ">$hostgroupfile" or die "Can't open $hostgroupfile for writing: $!";
+printhostgrpConfigData($stack, @allhosts);
+close ('HGOFH');
 
-      my $hostname = shift;
-      my $ip = shift;
-      my $tier = shift;
-      my $stack = shift;
-      my $alias;
+sub printhostsConfigData {
 
-      ($alias = $hostname) =~ s/-/\ /g; 
+  my $hostname = shift;
+  my $ip = shift;
+  my $tier = shift;
+  my $stack = shift;
+  my $alias;
 
-      print OFH "# Defining host entry for $hostname of $stack in $tier\n";
-      print OFH "define host{" . "\n";
-      print OFH "\thost_name\t$hostname\n";
-      print OFH "\tuse\t\t" . $stack . "-Generic-Host\n";
-      print OFH "\talias\t\t" . $alias . "\n";
-      print OFH "\taddress\t\t" . $ip . "\n"; 
-      print OFH "}\n";
-      return;
+  ($alias = $hostname) =~ s/-/\ /g; 
+
+  print OFH "# Defining host entry for $hostname of $stack in $tier\n";
+  print OFH "define host{" . "\n";
+  print OFH "\thost_name\t$hostname\n";
+  print OFH "\tuse\t\t" . "Generic-Host\n";
+  print OFH "\talias\t\t" . $alias . "\n";
+  print OFH "\taddress\t\t" . $ip . "\n"; 
+  print OFH "}\n";
+  return;
 }
 
+sub printhostgrpConfigData {
+  my $stack = shift;
+  my @allhosts = @_;
+  my $lstack = lc($stack);
+
+  print HGOFH "define hostgroup{" . "\n";
+  print HGOFH "hostgroup_name\t" . $lstack . "-all\n";
+  print HGOFH "alias\t\t" . "All " . $stack . "servers\n";
+  print HGOFH "members\t\t" . join(",", @allhosts) . "\n";
+  print HGOFH "}\n";
+  return;
+}
